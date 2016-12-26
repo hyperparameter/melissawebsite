96148cc3b98e0659b5f8b59ef9d2b58eb5767194
Daoud Clarke <daoud.clarke@gmail.com>
2016-12-26 16:18:51 +0000

    Fix phone number submission.

--- phone.py
+++ phone.py
@@ -5,10 +5,6 @@ from google.appengine.api import mail
 
 class MainPage(webapp2.RequestHandler):
 
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
-
     def post(self):
         phone = cgi.escape(self.request.get('PHONE'))
         mail.send_mail(sender="Daoud Clarke <daoud.clarke@gmail.com>",
@@ -16,10 +12,10 @@ class MainPage(webapp2.RequestHandler):
                        subject="Contact request from %s" % phone,
                        body="""
 Good news! you have a request from: %s
-""" % address)
+                       """ % phone)
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Thanks! Someone will be in touch shortly.')
 
 application = webapp2.WSGIApplication([
-        ('/email.py', MainPage),
+        ('/phone', MainPage),
         ], debug=True)
